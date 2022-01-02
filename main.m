99b0f56f14707b5f4e7ae7747147d5cc53f4ab9a
unknown <raltsolak@gmail.com>
2022-01-02 18:07:36 +0200

    added main file

---
+++ main.m
@@ -0,0 +1,17 @@
+%% Close all figures, clear workspace and console
+close all;
+clear;
+clc;
+
+%% Simulating the Binary Symmetric Channel
+
+% Initialize error prob & data
+er = 0.1;
+
+len = 10;
+data = randi([0, 1], 1, len);
+
+[ndata,err] = bsc(data,er);
+
+
+
