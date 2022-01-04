1afec515a7afcc2fe8f75583af419aaa4a074bfa
unknown <raltsolak@gmail.com>
2022-01-04 21:18:30 +0200

    04-Jan-22 9:17PM

---
+++ check_node.m
@@ -0,0 +1,21 @@
+function y = check_node(H, n, lk)
+
+    y = zeros(2, size(H,2));
+
+    for i = 1:size(H,2)
+        
+        if H(n,i) == 1
+            
+           
+            y(1,i) = lk(i);
+            y(2,i) = i;
+           
+        end
+        
+    end
+   
+    
+    y = [nonzeros(y(1,:))'; nonzeros(y(2,:))'];
+    
+end
+
